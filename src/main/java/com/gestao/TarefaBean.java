2d68db53926c6256671e8534a04d4f9d8226e091
poliana-dev <m.polisys02@gmail.com>
2025-10-08 09:01:38 -0300

    refactor: corrige estrutura de pacotes
    antes estava 'com.gestao/arquivos', agora está: 'com/gestao/arquivos'

--- src/main/java/com/gestao/TarefaBean.java
+++ src/main/java/com/gestao/TarefaBean.java
@@ -1,6 +1,5 @@
 package com.gestao;
 
-import jakarta.enterprise.context.RequestScoped;
 import jakarta.faces.application.FacesMessage;
 import jakarta.faces.context.FacesContext;
 import jakarta.faces.view.ViewScoped;
